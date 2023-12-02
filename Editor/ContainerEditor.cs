d9b8c070c315a56f20a7ac43451555d04adf20e8
Benedek Mészáros <149479346+benedekmeszaros@users.noreply.github.com>
2023-12-02 10:12:07 +0100

    Update ContainerEditor.cs

--- Editor/ContainerEditor.cs
+++ Editor/ContainerEditor.cs
@@ -6,37 +6,46 @@ namespace HexTools.UI.Components.Editor
     [CustomEditor(typeof(Container))]
     public class ContainerEditor : UnityEditor.Editor
     {
-        private readonly string[] options = { "Auto", "Clamped", "Constant", "Fit Content" };
+        private readonly string[] options = { "Auto", "Constant", "Fit Content" };
         private int widthSelect = -1;
         private int heightSelect = -1;
         private Container container;
-        private SerializedProperty width;
-        private SerializedProperty minWidth;
-        private SerializedProperty maxWidth;
-        private SerializedProperty height;
-        private SerializedProperty minHeight;
-        private SerializedProperty maxHeight;
+        [SerializeField] private SerializedProperty width;
+        [SerializeField] private SerializedProperty minWidth;
+        [SerializeField] private SerializedProperty maxWidth;
+        [SerializeField] private SerializedProperty isClampedHorizontally;
+        [SerializeField] private SerializedProperty height;
+        [SerializeField] private SerializedProperty minHeight;
+        [SerializeField] private SerializedProperty maxHeight;
+        [SerializeField] private SerializedProperty isClampedVertically;
 
         public override void OnInspectorGUI()
         {
             widthSelect = EditorGUILayout.Popup("Set Width", widthSelect, options);
             EditorGUI.indentLevel++;
-            if (widthSelect == 1)
+
+            if (widthSelect == 0)
             {
                 serializedObject.Update();
-                width.floatValue = -1;
-                DrawHorizontalPadding();
-                EditorGUILayout.PropertyField(minWidth);
-                EditorGUILayout.PropertyField(maxWidth);
-                if (minWidth.floatValue < 0)
-                    minWidth.floatValue = 0;
-                if (maxWidth.floatValue <= 0)
-                    maxWidth.floatValue = 1.0f;
-                if (maxWidth.floatValue < minWidth.floatValue)
-                    maxWidth.floatValue = minWidth.floatValue + 1.0f;
+                width.floatValue = 0;
+                DrawHorizontalMargine();
+                bool clamped = isClampedHorizontally.boolValue;
+                clamped = isClampedHorizontally.boolValue = EditorGUILayout.Toggle("Clamped", clamped);
+                if (clamped)
+                {
+                    EditorGUILayout.PropertyField(minWidth);
+                    EditorGUILayout.PropertyField(maxWidth);
+                    if (minWidth.floatValue < 0)
+                        minWidth.floatValue = 0;
+                    if (maxWidth.floatValue <= 0)
+                        maxWidth.floatValue = 1.0f;
+                    if (maxWidth.floatValue < minWidth.floatValue)
+                        maxWidth.floatValue = minWidth.floatValue + 1.0f;
+                }
                 serializedObject.ApplyModifiedProperties();
             }
-            else if (widthSelect == 2)
+            else
+            if (widthSelect == 1)
             {
                 serializedObject.Update();
                 float currWidth = EditorGUILayout.FloatField("Width", width.floatValue);
@@ -47,38 +56,53 @@ namespace HexTools.UI.Components.Editor
                     maxWidth.floatValue = currWidth;
                 serializedObject.ApplyModifiedProperties();
             }
-            else if (widthSelect == 3)
+            else
+            if (widthSelect == 2)
             {
                 serializedObject.Update();
-                width.floatValue = -2;
-                serializedObject.ApplyModifiedProperties();
-            }
-            else if (widthSelect == 0)
-            {
-                serializedObject.Update();
-                width.floatValue = 0;
-                DrawHorizontalPadding();
+                width.floatValue = -1;
+                bool clamped = isClampedHorizontally.boolValue;
+                clamped = isClampedHorizontally.boolValue = EditorGUILayout.Toggle("Clamped", clamped);
+                if (clamped)
+                {
+                    EditorGUILayout.PropertyField(minWidth);
+                    EditorGUILayout.PropertyField(maxWidth);
+                    if (minWidth.floatValue < 0)
+                        minWidth.floatValue = 0;
+                    if (maxWidth.floatValue <= 0)
+                        maxWidth.floatValue = 1.0f;
+                    if (maxWidth.floatValue < minWidth.floatValue)
+                        maxWidth.floatValue = minWidth.floatValue + 1.0f;
+                }
                 serializedObject.ApplyModifiedProperties();
             }
             EditorGUI.indentLevel--;
+
             heightSelect = EditorGUILayout.Popup("Set Height", heightSelect, options);
             EditorGUI.indentLevel++;
-            if (heightSelect == 1)
+
+            if (heightSelect == 0)
             {
                 serializedObject.Update();
-                height.floatValue = -1;
-                DrawVerticalPadding();
-                EditorGUILayout.PropertyField(minHeight);
-                EditorGUILayout.PropertyField(maxHeight);
-                if (minHeight.floatValue < 0)
-                    minHeight.floatValue = 0;
-                if (maxHeight.floatValue <= 0)
-                    maxHeight.floatValue = 1.0f;
-                if (maxHeight.floatValue < minHeight.floatValue)
-                    maxHeight.floatValue = minHeight.floatValue + 1.0f;
+                height.floatValue = 0;
+                DrawVerticalMargine();
+                bool clamped = isClampedVertically.boolValue;
+                clamped = isClampedVertically.boolValue = EditorGUILayout.Toggle("Clamped", clamped);
+                if (clamped)
+                {
+                    EditorGUILayout.PropertyField(minHeight);
+                    EditorGUILayout.PropertyField(maxHeight);
+                    if (minHeight.floatValue < 0)
+                        minHeight.floatValue = 0;
+                    if (maxHeight.floatValue <= 0)
+                        maxHeight.floatValue = 1.0f;
+                    if (maxHeight.floatValue < minHeight.floatValue)
+                        maxHeight.floatValue = minHeight.floatValue + 1.0f;
+                }
                 serializedObject.ApplyModifiedProperties();
             }
-            else if (heightSelect == 2)
+            else
+            if (heightSelect == 1)
             {
                 serializedObject.Update();
                 float currHeight = EditorGUILayout.FloatField("Heigth", height.floatValue);
@@ -89,19 +113,27 @@ namespace HexTools.UI.Components.Editor
                     maxHeight.floatValue = currHeight;
                 serializedObject.ApplyModifiedProperties();
             }
-            else if (heightSelect == 3)
+            else
+            if (heightSelect == 2)
             {
                 serializedObject.Update();
-                height.floatValue = -2;
+                height.floatValue = -1;
+                bool clamped = isClampedVertically.boolValue;
+                clamped = isClampedVertically.boolValue = EditorGUILayout.Toggle("Clamped", clamped);
+                if (clamped)
+                {
+                    EditorGUILayout.PropertyField(minHeight);
+                    EditorGUILayout.PropertyField(maxHeight);
+                    if (minHeight.floatValue < 0)
+                        minHeight.floatValue = 0;
+                    if (maxHeight.floatValue <= 0)
+                        maxHeight.floatValue = 1.0f;
+                    if (maxHeight.floatValue < minHeight.floatValue)
+                        maxHeight.floatValue = minHeight.floatValue + 1.0f;
+                }
                 serializedObject.ApplyModifiedProperties();
             }
-            else if (heightSelect == 0)
-            {
-                serializedObject.Update();
-                height.floatValue = 0;
-                DrawVerticalPadding();
-                serializedObject.ApplyModifiedProperties();
-            }
+
             EditorGUI.indentLevel--;
         }
 
@@ -113,20 +145,22 @@ namespace HexTools.UI.Components.Editor
             width = serializedObject.FindProperty("width");
             minWidth = serializedObject.FindProperty("minWidth");
             maxWidth = serializedObject.FindProperty("maxWidth");
+            isClampedHorizontally = serializedObject.FindProperty("isClampedHorizontally");
             height = serializedObject.FindProperty("height");
             minHeight = serializedObject.FindProperty("minHeight");
             maxHeight = serializedObject.FindProperty("maxHeight");
+            isClampedVertically = serializedObject.FindProperty("isClampedVertically");
 
             widthSelect = ValueToSelect((int)width.floatValue);
             heightSelect = ValueToSelect((int)height.floatValue);
 
-            if (width.floatValue == -3.0f)
+            if (width.floatValue == -2.0f)
             {
                 RectTransform rectTransform = container.transform as RectTransform;
                 width.floatValue = rectTransform.rect.width;
                 serializedObject.ApplyModifiedProperties();
             }
-            if (height.floatValue == -3.0f)
+            if (height.floatValue == -2.0f)
             {
                 RectTransform rectTransform = container.transform as RectTransform;
                 height.floatValue = rectTransform.rect.height;
@@ -135,27 +169,24 @@ namespace HexTools.UI.Components.Editor
         }
         private int ValueToSelect(int value)
         {
+            if (value == -1)
+                return 2;
+            else
             if (value == 0)
                 return 0;
             else
-            if (value == -1)
                 return 1;
-            else
-            if (value == -2)
-                return 3;
-            else
-                return 2;
         }
-        private void DrawHorizontalPadding()
+        private void DrawHorizontalMargine()
         {
             bool foldout = SessionState.GetBool("container-horizontal-offset-foldout", false);
-            SessionState.SetBool("container-horizontal-offset-foldout", EditorGUILayout.Foldout(foldout, "Padding"));
+            SessionState.SetBool("container-horizontal-offset-foldout", EditorGUILayout.Foldout(foldout, "Margine"));
             if (foldout)
             {
                 EditorGUI.indentLevel++;
                 EditorGUI.BeginChangeCheck();
-                container.Padding.left = EditorGUILayout.IntField("Left", container.Padding.left);
-                container.Padding.right = EditorGUILayout.IntField("Right", container.Padding.right);
+                container.Margine.left = EditorGUILayout.IntField("Left", container.Margine.left);
+                container.Margine.right = EditorGUILayout.IntField("Right", container.Margine.right);
                 if (EditorGUI.EndChangeCheck())
                 {
                     container.SetLayoutHorizontal();
@@ -164,16 +195,16 @@ namespace HexTools.UI.Components.Editor
                 EditorGUI.indentLevel--;
             }
         }
-        private void DrawVerticalPadding()
+        private void DrawVerticalMargine()
         {
             bool foldout = SessionState.GetBool("container-vertical-offset-foldout", false);
-            SessionState.SetBool("container-vertical-offset-foldout", EditorGUILayout.Foldout(foldout, "Padding"));
+            SessionState.SetBool("container-vertical-offset-foldout", EditorGUILayout.Foldout(foldout, "Margine"));
             if (foldout)
             {
                 EditorGUI.indentLevel++;
                 EditorGUI.BeginChangeCheck();
-                container.Padding.bottom = EditorGUILayout.IntField("Bottom", container.Padding.bottom);
-                container.Padding.top = EditorGUILayout.IntField("Top", container.Padding.top);
+                container.Margine.bottom = EditorGUILayout.IntField("Bottom", container.Margine.bottom);
+                container.Margine.top = EditorGUILayout.IntField("Top", container.Margine.top);
                 if (EditorGUI.EndChangeCheck())
                 {
                     container.SetLayoutVertical();
