12988de6b7d12f3597c5f394381f5e48b7d25115
Benedek Mészáros <149479346+benedekmeszaros@users.noreply.github.com>
2023-12-02 10:13:17 +0100

    Update Container.cs
    Latest stabile version.

--- Runtime/Container.cs
+++ Runtime/Container.cs
@@ -14,10 +14,8 @@ namespace HexTools.UI.Components
             private float top;
             private float bottom;
 
-            public Offset()
-            {
+            public Offset() { }
 
-            }
             public Offset(float left, float right, float top, float bottom)
             {
                 this.left = left;
@@ -35,10 +33,12 @@ namespace HexTools.UI.Components
         [SerializeField] private float width = -3;
         [SerializeField] private float minWidth;
         [SerializeField] private float maxWidth;
+        [SerializeField] private bool isClampedHorizontally;
         [SerializeField] private float height = -3;
         [SerializeField] private float minHeight;
         [SerializeField] private float maxHeight;
-        [SerializeField] private RectOffset padding;
+        [SerializeField] private bool isClampedVertically;
+        [SerializeField] private RectOffset margine;
         private readonly Offset offsetBackup = new Offset();
         [System.NonSerialized] private RectTransform m_Rect;
 #pragma warning disable 649
@@ -60,7 +60,7 @@ namespace HexTools.UI.Components
         public float Height { get => height; set => height = value; }
         public float MinHeight { get => minHeight; set => minHeight = value; }
         public float MaxHeight { get => maxHeight; set => maxHeight = value; }
-        public RectOffset Padding { get => padding; set => padding = value; }
+        public RectOffset Margine { get => margine; set => margine = value; }
 
         public void SetLayoutHorizontal()
         {
@@ -83,8 +83,8 @@ namespace HexTools.UI.Components
         {
             base.OnEnable();
             m_Rect = transform as RectTransform;
-            if (padding == null)
-                padding = new RectOffset(0, 0, 0, 0);
+            if (margine == null)
+                margine = new RectOffset(0, 0, 0, 0);
             SetDirty();
         }
         protected override void OnDisable()
@@ -106,7 +106,7 @@ namespace HexTools.UI.Components
 
         private void CalculateRect()
         {
-            if (rectTransform.hasChanged && padding != null)
+            if (rectTransform.hasChanged && margine != null)
             {
                 Vector2 pivot = rectTransform.pivot;
                 offsetBackup.Left = rectTransform.offsetMin.x;
@@ -116,46 +116,50 @@ namespace HexTools.UI.Components
                 rectTransform.pivot = new Vector2(0.5f, 0.5f);
                 rectTransform.anchorMin = new Vector2(0, 0);
                 rectTransform.anchorMax = new Vector2(1, 1);
-                Rect rect = rectTransform.rect;
                 m_Tracker.Add(this, rectTransform, DrivenTransformProperties.Anchors);
                 m_Tracker.Add(this, rectTransform, DrivenTransformProperties.SizeDelta);
+
                 if (width == -1)
                 {
-                    offsetBackup.Left = padding.left;
-                    offsetBackup.Right = -padding.right;
-                    rectTransform.offsetMin = new Vector2(offsetBackup.Left, offsetBackup.Bottom);
-                    rectTransform.offsetMax = new Vector2(offsetBackup.Right, offsetBackup.Top);
-                    rect = rectTransform.rect;
-                    m_Tracker.Add(this, rectTransform, DrivenTransformProperties.AnchoredPositionX);
-                    if (rect.width > maxWidth)
-                    {
-                        float offset = rect.width - maxWidth;
-                        offsetBackup.Left += offset / 2;
-                        offsetBackup.Right -= offset / 2;
-                    }
+                    float preferredWidth = LayoutUtility.GetPreferredSize(m_Rect, 0);
+                    if (isClampedHorizontally && preferredWidth > maxWidth)
+                        rectTransform.SetSizeWithCurrentAnchors(0, maxWidth);
+                    else if (isClampedHorizontally && preferredWidth < minWidth)
+                        rectTransform.SetSizeWithCurrentAnchors(0, minWidth);
                     else
-                    if (rect.width < minWidth)
-                    {
-                        float offset = minWidth - rect.width;
-                        offsetBackup.Left -= offset / 2;
-                        offsetBackup.Right += offset / 2;
-                    }
-                }
-                else
-                if (width == 0)
-                {
-                    offsetBackup.Left = padding.left;
-                    offsetBackup.Right = -padding.right;
-                    m_Tracker.Add(this, rectTransform, DrivenTransformProperties.AnchoredPositionX);
-                }
-                else if (width == -2)
-                {
-                    rectTransform.SetSizeWithCurrentAnchors(0, LayoutUtility.GetPreferredSize(m_Rect, 0));
+                        rectTransform.SetSizeWithCurrentAnchors(0, preferredWidth);
                     offsetBackup.Left = rectTransform.offsetMin.x;
                     offsetBackup.Right = rectTransform.offsetMax.x;
                 }
                 else
-                if (width > 0)
+                if (width == 0)
+                {
+                    offsetBackup.Left = margine.left;
+                    offsetBackup.Right = -margine.right;
+                    m_Tracker.Add(this, rectTransform, DrivenTransformProperties.AnchoredPositionX);
+
+                    if (isClampedHorizontally)
+                    {
+                        rectTransform.offsetMin = new Vector2(offsetBackup.Left, offsetBackup.Bottom);
+                        rectTransform.offsetMax = new Vector2(offsetBackup.Right, offsetBackup.Top);
+                        float currentWidth = rectTransform.rect.width;
+
+                        if (currentWidth > maxWidth)
+                        {
+                            float offset = currentWidth - maxWidth;
+                            offsetBackup.Left += offset / 2;
+                            offsetBackup.Right -= offset / 2;
+                        }
+                        else
+                    if (currentWidth < minWidth)
+                        {
+                            float offset = minWidth - currentWidth;
+                            offsetBackup.Left -= offset / 2;
+                            offsetBackup.Right += offset / 2;
+                        }
+                    }
+                }
+                else
                 {
                     rectTransform.SetSizeWithCurrentAnchors(0, width);
                     offsetBackup.Left = rectTransform.offsetMin.x;
@@ -164,40 +168,45 @@ namespace HexTools.UI.Components
 
                 if (height == -1)
                 {
-                    offsetBackup.Bottom = padding.bottom;
-                    offsetBackup.Top = -padding.top;
-                    rectTransform.offsetMin = new Vector2(offsetBackup.Left, offsetBackup.Bottom);
-                    rectTransform.offsetMax = new Vector2(offsetBackup.Right, offsetBackup.Top);
-                    rect = rectTransform.rect;
-                    m_Tracker.Add(this, rectTransform, DrivenTransformProperties.AnchoredPositionY);
-                    if (rect.height > maxHeight)
-                    {
-                        float offset = rect.height - maxHeight;
-                        offsetBackup.Bottom += offset / 2;
-                        offsetBackup.Top -= offset / 2;
-                    }
+                    float preferredHeight = LayoutUtility.GetPreferredSize(m_Rect, 1);
+                    if (isClampedVertically && preferredHeight > maxHeight)
+                        rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)1, maxHeight);
+                    else if (isClampedVertically && preferredHeight < minHeight)
+                        rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)1, minHeight);
                     else
-                    if (rect.height < minHeight)
-                    {
-                        float offset = minHeight - rect.height;
-                        offsetBackup.Bottom -= offset / 2;
-                        offsetBackup.Top += offset / 2;
-                    }
-                }
-                else if (height == 0)
-                {
-                    offsetBackup.Bottom = padding.bottom;
-                    offsetBackup.Top = -padding.top;
-                    m_Tracker.Add(this, rectTransform, DrivenTransformProperties.AnchoredPositionY);
-                }
-                else
-                if (height == -2)
-                {
-                    rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)1, LayoutUtility.GetPreferredSize(m_Rect, 1));
+                        rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)1, preferredHeight);
                     offsetBackup.Bottom = rectTransform.offsetMin.y;
                     offsetBackup.Top = rectTransform.offsetMax.y;
                 }
-                if (height > 0)
+                else
+                if (height == 0)
+                {
+                    offsetBackup.Bottom = margine.bottom;
+                    offsetBackup.Top = -margine.top;
+                    m_Tracker.Add(this, rectTransform, DrivenTransformProperties.AnchoredPositionY);
+
+                    if (isClampedVertically)
+                    {
+                        rectTransform.offsetMin = new Vector2(offsetBackup.Left, offsetBackup.Bottom);
+                        rectTransform.offsetMax = new Vector2(offsetBackup.Right, offsetBackup.Top);
+                        float currentHeight = rectTransform.rect.height;
+
+                        if (currentHeight > maxHeight)
+                        {
+                            float offset = currentHeight - maxHeight;
+                            offsetBackup.Bottom += offset / 2;
+                            offsetBackup.Top -= offset / 2;
+                        }
+                        else
+                    if (currentHeight < minHeight)
+                        {
+                            float offset = minHeight - currentHeight;
+                            offsetBackup.Bottom -= offset / 2;
+                            offsetBackup.Top += offset / 2;
+                        }
+                    }
+                }
+                else
                 {
                     rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)1, height);
                     offsetBackup.Bottom = rectTransform.offsetMin.y;
